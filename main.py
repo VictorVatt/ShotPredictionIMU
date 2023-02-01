84a7ec62e4c342ead32ea9b6e5e108b1128ee636
Vatt <90685169+VictorVatt@users.noreply.github.com>
2023-02-01 22:55:12 +0100

    load acceleration data and gyroscope from .mat file

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 
 data = loadmat("data/new_data_all_7.mat")
-
 data = np.array(data["all_data"], dtype=object)
+
 
 N_sujet = 0
 N_coup = 0
@@ -15,10 +15,6 @@ N_coup = 0
 data = data[N_sujet][0][N_coup][0]
 
 
-
-
 df = pd.DataFrame(data, columns=["AccX", "AccY", "AccZ", "GyrX", "GyrY", "GyrZ"])
-
-
 
 print(df)
