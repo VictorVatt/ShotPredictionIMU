877f79d969a1159712466f2289f5817f4b6e7386
Vatt <90685169+VictorVatt@users.noreply.github.com>
2023-02-06 14:00:15 +0100

    load acceleration data and gyroscope from .mat file

---
+++ format_dataset.py
@@ -0,0 +1,43 @@
+from mat4py import loadmat
+import pandas as pd
+import numpy as np
+from scipy.stats import kurtosis, skew
+import matplotlib.pyplot as plt
+
+# on importe les données du fichier .mat format de données matlab
+data = loadmat("data/new_data_all_7.mat")
+
+data_label = np.array(data["all_data_label"], dtype=object)
+data = np.array(data["all_data"], dtype=object)
+
+cols_labels = ["AccXMean", "AccXSD", "AccXSkew", "AccXKurtosis", "AccXMin", "AccXMax", "AccYMean", "AccYSD", "AccYSkew", "AccYKurtosis", "AccYMin", "AccYMax", "AccZMean", "AccZSD", "AccZSkew", "AccZKurtosis", "AccZMin", "AccZMax",
+               "GyrXMean", "GyrXSD", "GyrXSkew", "GyrXKurtosis", "GyrXMin", "GyrXMax", "GyrYMean", "GyrYSD", "GyrYSkew", "GyrYKurtosis", "GyrYMin", "GyrYMax", "GyrZMean", "GyrZSD", "GyrZSkew", "GyrZKurtosis", "GyrZMin", "GyrZMax", "TypeOfShot"]
+
+# df = pd.DataFrame(data, columns=["AccX", "AccY", "AccZ", "GyrX", "GyrY", "GyrZ"])
+# df_label = pd.DataFrame(data_label, columns=["Stroke Type", "OutSpeed", "OutSpinSpeed", "IncomingSpeed", "IncomingSpinSpeed"])
+
+df = pd.DataFrame(columns=cols_labels)
+
+
+for N_sujet in range(len(data)):
+    for N_coup in range(len(data[N_sujet][0])):
+        donnee = np.array(data[N_sujet][0][N_coup][0])
+        donne_label = np.array(data_label[N_sujet][0][N_coup])
+        row = list()
+        for N_cols in range(6):
+            mean = np.mean(donnee[:, N_cols])
+            sd = np.std(donnee[:, N_cols])
+            skewness = skew(donnee[:, N_cols])
+            kurtosisness = kurtosis(donnee[:, N_cols])
+            minimum = np.min(donnee[:, N_cols])
+            maximum = np.max(donnee[:, N_cols])
+            row.append(mean)
+            row.append(sd)
+            row.append(skewness)
+            row.append(kurtosisness)
+            row.append(minimum)
+            row.append(maximum)
+        row.append(donne_label[0][0])
+        df.loc[len(df)] = row
+
+df.to_csv("training_dataset")
--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-from mat4py import loadmat
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-data = loadmat("data/new_data_all_7.mat")
-data = np.array(data["all_data"], dtype=object)
-
-
-N_sujet = 0
-N_coup = 0
-
-
-data = data[N_sujet][0][N_coup][0]
-
-
-df = pd.DataFrame(data, columns=["AccX", "AccY", "AccZ", "GyrX", "GyrY", "GyrZ"])
-
-print(df)
